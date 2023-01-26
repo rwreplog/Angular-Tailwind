b2fbca8a87b8d57eed599002d66926775f5104f1
Ryan Replogle <ryan.replogle@gmail.com>
2023-01-26 13:57:17 -0500

    linting changes

--- angular-tailwind/projects/angular-tailwind/src/app/app.component.ts
+++ angular-tailwind/projects/angular-tailwind/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'at-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-tailwind';
