28b58317ee879839d149c5c4e4d9bca9cbfa21b6
Ryan Replogle <ryan.replogle@gmail.com>
2023-01-27 16:59:02 -0500

    updates

--- angular-tailwind/tailwind.config.js
+++ angular-tailwind/tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.{html,ts}", "./projects/**/*.{html,ts}"],
+  content: ['./src/**/*.{html,ts}', './projects/**/*.{html,ts}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {},
   },
   plugins: [],
